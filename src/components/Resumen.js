006d8ab29e8e5164cf244d481de5d8bcbc7021dc
Hernan <hernansamek@gmail.com>
2020-09-08 14:06:14 -0300

    Initial commit

---
+++ src/components/Resumen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import {primerMayuscula} from '../herlper';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00083F;
+    color: #FFFFFF;
+    margin-top: 1rem;
+`;
+
+const Resumen = ({datos}) => {
+    //extraer datos
+    const {marca, year, plan} = datos;
+
+    if( marca === '' || year === '' || plan === '')
+    {
+        return null;
+    }
+
+    return ( 
+        <ContenedorResumen>
+            <h2>Resumen de cotizacion</h2>
+        <ul>
+            <li>Marca: {primerMayuscula(marca)}</li>
+            <li>Plan: {primerMayuscula(plan)}</li>
+            <li>A&ntilde;o: {year}</li>
+        </ul>
+        </ContenedorResumen>
+        
+     );
+}
+
+Resumen.propTypes = {
+    datos: PropTypes.object.isRequired
+}
+export default Resumen;
